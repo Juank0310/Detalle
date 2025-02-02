5b99a16e9f04cd6879c3987087f35ddfeb8aa064
Juank0310 <juankael10@gmail.com>
2025-02-02 00:43:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let imagenes = ["imagenes/1.jpg", "imagenes/2.jpg", "imagenes/3.jpg"]; // Agrega aquí tus imágenes
+let imagenes = ["imagenes/1.jpg", "imagenes/2.jpg", "imagenes/3.jpg", "imagenes/4.jpg", "imagenes/5.jpg", "imagenes/6.jpg", "imagenes/7.jpg", "imagenes/8.jpg", "imagenes/9.jpg","imagenes/10.jpg", "imagenes/11.jpg", "imagenes/12.jpg", "imagenes/13.jpg", "imagenes/14.jpg","imagenes/15.jpg", "imagenes/16.jpg", "imagenes/17.jpg", "imagenes/18.jpg","imagenes/19.jpg", "imagenes/20.jpg", "imagenes/21.jpg", "imagenes/22.jpg", "imagenes/23.jpg","imagenes/24.jpg", "imagenes/25.jpg", "imagenes/26.jpg"]; // Agrega aquí tus imágenes
 let indice = 0;
 
 function cambiarImagen(direccion) {
